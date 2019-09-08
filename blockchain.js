93d0b1fa1c5eb7cff4ce45b103551ac62107ecb4
Adarsh Thapa <realadarshthapa@gmail.com>
2019-09-08 21:18:02 +0545

    Added createNewBlock method

--- blockchain.js
+++ blockchain.js
@@ -1,6 +1,20 @@
-class Blockchain {
-    constructor() {
-        this.chain = [];
-        this.newTransactions = [];
-    }
+function Blockchain() {
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions: this.newTransactions,
+        nonce: nonce,
+        hash: hash,
+        previousBlockHash: previousBlockHash
+    };
+
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
 }
