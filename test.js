76087253347586ab67bb0240e2b7215500e4c545
Adarsh Thapa <realadarshthapa@gmail.com>
2019-09-09 11:49:27 +0545

    Added test for hashBlock method

--- test.js
+++ test.js
@@ -1,17 +1,25 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(93424, '32akjdf3k392kl234u', '4kjkd9ka9a9sk323w5');
+const previousBlockHash = '7sdfjsdf8sduk39u3kj239kkjdf323j4'
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'ADARSHksdlkjf923k23j43',
+        recipient: 'MARK872342k34234982324'
+    },
+    {
+        amount: 50,
+        sender: '8sf9sfkjfkdsjf98u23n2n3',
+        recipient: 'kldfja9sfd8ui2j3k923332'
+    },
+    {
+        amount: 100,
+        sender: 'skdfjsao823il2j39832',
+        recipient: 'sdjfio32i2j3iojd233'
+    }
+];
 
-bitcoin.createNewTransaction(100, 'ADARSH9d8sdkfs9sdf', 'MARK8sdfksdf83k328');
+const nonce = 500;
 
-bitcoin.createNewBlock(79890, '088dfkdsk339s3l232', '532mn32349iuj8s353');
-
-bitcoin.createNewTransaction(70, 'a3423dfi44843523j3', '34jk4a93kj29ia4742');
-bitcoin.createNewTransaction(500, '45kljkad83nsf8sdf3', '09lkjlkdfsdfkksf31');
-bitcoin.createNewTransaction(1500, '923jlas9ijja3kdk2j', '234jlks83j0343233d');
-
-bitcoin.createNewBlock(903825, 'k8s4kad32432k30ljl', '532mn32349iuj8s33r');
-
-console.log(bitcoin);
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
