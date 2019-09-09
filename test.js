17ad44330e5a1a720dbd7a84a2e5d430afa24bcd
Adarsh Thapa <realadarshthapa@gmail.com>
2019-09-09 11:13:34 +0545

    Added test for createNewBlock & createNewTransaction methods

--- test.js
+++ test.js
@@ -2,8 +2,16 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(7839, 'a3423dfi44843523kdj3', '34jk4a93kj29jk3oia47');
-bitcoin.createNewBlock(3424, '45kljkad83nsf8lasdf3', '09lkjlkdfsdfkj4j4ksf');
-bitcoin.createNewBlock(1232, '923jlas9ijja3kkadk2j', '234jlks83jsaijs34323');
+bitcoin.createNewBlock(93424, '32akjdf3k392kl234u', '4kjkd9ka9a9sk323w5');
+
+bitcoin.createNewTransaction(100, 'ADARSH9d8sdkfs9sdf', 'MARK8sdfksdf83k328');
+
+bitcoin.createNewBlock(79890, '088dfkdsk339s3l232', '532mn32349iuj8s353');
+
+bitcoin.createNewTransaction(70, 'a3423dfi44843523j3', '34jk4a93kj29ia4742');
+bitcoin.createNewTransaction(500, '45kljkad83nsf8sdf3', '09lkjlkdfsdfkksf31');
+bitcoin.createNewTransaction(1500, '923jlas9ijja3kdk2j', '234jlks83j0343233d');
+
+bitcoin.createNewBlock(903825, 'k8s4kad32432k30ljl', '532mn32349iuj8s33r');
 
 console.log(bitcoin);
